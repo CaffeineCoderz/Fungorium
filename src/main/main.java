035791af54c579a899649ff1313f844954644c40
Farago Aron <farago.aron.03@edu.bme.hu>
2025-03-14 12:01:49 +0100

    szoltárok 2:2

--- src/main/main.java
+++ src/main/main.java
@@ -1,6 +1,6 @@
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Fungorium szimuláció NEM akart, de elindult!");
+        System.out.println("Fungorium szimuláció NEM akart és nem fog elindult!");
     }
 }
