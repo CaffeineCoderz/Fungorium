00b18b2880f2b7b3d5b7efc43ffbb7fb62fc7b16
Diviki Levente <divikilevi13@gmail.com>
2025-03-22 15:49:32 +0100

    DecreasingTekton 90%Done

--- src/model/tektonTypes/DecreasingTekton.java
+++ src/model/tektonTypes/DecreasingTekton.java
@@ -1,9 +1,25 @@
 package tektonTypes;
 
+import insect.Insect;
+
 public class DecreasingTekton extends Tekton{
      public DecreasingTekton(){
         super(true, true);
     }
 
-        
+    @Override
+    public void addInsect(Insect insect){
+        if (!insects.contains(insect)) {
+            insect.setDecrease(true);
+            insects.add(insect);
+        }
+    }  
+    @Override
+    public void removeInsect(Insect insect){
+        if (!insects.contains(insect) || insects.isEmpty()) {
+            return;
+        }
+        insect.setDecrease(false);
+        insects.remove(insect);
+    }    
 }
