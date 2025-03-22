c79a2bf1d7a1f8db79e9f23393ba7875b7ef26a6
Diviki Levente <divikilevi13@gmail.com>
2025-03-22 16:01:30 +0100

    Spore class implementations

--- src/model/sporeTypes/Spore.java
+++ src/model/sporeTypes/Spore.java
@@ -7,17 +7,31 @@ public class Spore {
     private Integer nutritionValue;
     private Tekton myTekton;
 
+    public Spore(){
+        nutritionValue = 10;
+        myTekton = null;
+    }
+    public Spore(Tekton t,Integer nutval){
+        myTekton = t;
+        nutritionValue = nutval;
+    }
+
     public void consume(Insect insect) {
-        // insect.consumeSpore(this);
+        insect.consumeSpore(this);
+        absorbed();
     }
 
     public void setTekton(Tekton tekton) {
         myTekton = tekton;
     }
 
-    // ToDo mit jelent itt az absorbed? Implementáció
+    //Ez a Spore példány pusztulását segíti. Leginkább azért kell, mivel tekton törésnél és elfogyasztásnál is ugyanazok a folyamatok mennek végbe.
     public void absorbed() {
+        myTekton.removeSpore(this);
         myTekton = null;
         
+    }
+    public Integer getNutValue(){
+        return nutritionValue;
     }
 }
