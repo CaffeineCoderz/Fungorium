3f680235c85359410c15cd8a61c3883b098703d5
Farago Aron <farago.aron.03@edu.bme.hu>
2025-03-24 21:45:05 +0100

    javadoc for random genrator

--- src/utils/RandomGenerator.java
+++ src/utils/RandomGenerator.java
@@ -1,6 +1,12 @@
 package utils;
 
 public class RandomGenerator {
+    /**
+     * Generates a random number between two values.
+     * 
+     * @param min the minimum value of the random number
+     * @param max the maximum value of the random number
+     */
     public static Integer generateRandomNumber(Integer min, Integer max){
         return (int)(Math.random() * ((max - min) + 1)) + min;
     }
