d160c9895be48be9fd5e4cecb4f1463c3e82e931
Mador <dominik.andorka@gmail.com>
2025-03-27 13:46:50 +0100

    Add: MultiplyInsectSpore, FeedThreadTekton, CommandProcessor. Change: Test.sh Compile.sh + Comments

---
+++ src/model/sporeTypes/MultiplyInsectSpore.java
@@ -0,0 +1,8 @@
+package sporeTypes;
+
+public class MultiplyInsectSpore {
+    // Létezik olyan spóra, amelyik az őt megevő rovart osztódásra készteti.
+    // Ilyenkor keletkezik még egy, az előzőtől függetlenül élő rovar, akinek a
+    // rovarásza megegyezik az eredeti rovar rovarászával.
+
+}
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ rm -rf out
 
 # Fordítás
 mkdir -p out
-javac -d out test/Tests.java src/main.java src/utils/*.java src/model/fungus/*.java src/model/interfaces/*.java src/model/sporeTypes/*.java src/model/tektonTypes/*.java src/model/insect/*.java src/logic/*.java
+javac -d out test/Tests.java src/main.java src/utils/*.java src/model/fungus/*.java src/model/interfaces/*.java src/model/sporeTypes/*.java src/model/tektonTypes/*.java src/model/insect/*.java src/logic/*.java src/commands/*.java
 
 # Futtatás
 java -cp out test.Tests
