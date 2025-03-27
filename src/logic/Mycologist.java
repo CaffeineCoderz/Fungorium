129eb91712502766fb8b175cbd21d984842a6fa8
Csomós Áron Attila <165198233+CsomosAron@users.noreply.github.com>
2025-03-27 17:39:34 +0100

    Update Mycologist.java

--- src/logic/Mycologist.java
+++ src/logic/Mycologist.java
@@ -5,17 +5,8 @@ import fungus.FungusSpecies;
 import fungus.FungusThread;
 
 public class Mycologist {
-<<<<<<< HEAD
-<<<<<<< HEAD
     private FungusSpecies mySpecies;
-
-=======
-    FungusSpecies mySpecies;
->>>>>>> 0dffc08 (Mycologist majdnem kész)
-=======
-    private FungusSpecies mySpecies;
-
->>>>>>> 098513a6908733e28c5ce89264663b736b0fc360
+    
     public Mycologist(FungusSpecies species) {
         mySpecies = species;
     }
