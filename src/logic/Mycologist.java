0d73d464f0b13fa29006405ac924f3e7d9855a44
Csomós Áron Attila <csomosaa@edu.bme.hu>
2025-03-27 16:53:17 +0100

    Mycologist majdnem kész

--- src/logic/Mycologist.java
+++ src/logic/Mycologist.java
@@ -1,5 +1,32 @@
 package logic;
 
-public class Mycologist {
+import fungus.FungusBody;
+import fungus.FungusSpecies;
+import fungus.FungusThread;
 
+public class Mycologist {
+    FungusSpecies mySpecies;
+    public Mycologist(FungusSpecies species) {
+        mySpecies = species;
+    }
+
+    public FungusSpecies getSpecies() {
+        return mySpecies;
+    }
+
+    public void setSpecies(FungusSpecies species) {
+        mySpecies = species;
+    }
+    
+    public void grow(FungusThread thread) {
+        mySpecies.growBody(thread);
+    }
+
+    public void setThread(Tekton tekton, FungusThread selectedThread, FungusThread newThread) {
+        mySpecies.growThread(tekton, selectedThread, newThread);
+    }
+
+    public void killInsect(FungusThread thread) {
+        
+    }
 }
