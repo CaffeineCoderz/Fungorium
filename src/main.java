e628ee126eb054491f890bd4ce77f3273876d25b
Mador <dominik.andorka@gmail.com>
2025-04-18 12:17:33 +0200

    Add choosing between options on main

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import commands.CommandProcessor;
 import commands.Tester;
 import logic.GameLogic;
@@ -5,24 +6,44 @@ import logic.GameLogic;
 public class main {
 
     public static void main(String[] args) {
-        // CommandProcessor processor = new CommandProcessor();
-        // processor.processConfigText("config");
-        // processor.start();
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
 
-        Tester tester = new Tester();
-        tester.checkTestResults();
+        while (choice != 4) {
+            System.out.println("Choose an option:");
+            System.out.println("1. Process configuration and start CommandProcessor");
+            System.out.println("2. Run Tester to check test results");
+            System.out.println("3. Start the game");
+            System.out.println("4. Exit");
 
-        // Create an instance of GameLogic
-        GameLogic gameLogic = new GameLogic();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
 
-        // Set the game time (optional)
-        gameLogic.setGameTime(15);
+            switch (choice) {
+                case 1:
+                    CommandProcessor processor = new CommandProcessor();
+                    processor.processConfigText("config");
+                    processor.start();
+                    break;
+                case 2:
+                    Tester tester = new Tester();
+                    tester.checkTestResults();
+                    break;
+                case 3:
+                    GameLogic gameLogic = new GameLogic();
+                    gameLogic.setGameTime(15);
+                    System.out.println("Welcome to the game!");
+                    gameLogic.startGame();
+                    System.out.println("Game over!");
+                    break;
+                case 4:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
 
-        // Start the game
-        System.out.println("Welcome to the game!");
-        gameLogic.startGame();
-
-        // End of the game
-        System.out.println("Game over!");
+        scanner.close();
     }
 }
