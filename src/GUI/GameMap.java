3506d111911a89c2aa484b5a7c5601d75d4e6f80
Farago Aron <farago.aron.03@edu.bme.hu>
2025-04-22 23:14:40 +0200

    square cells

--- src/GUI/GameMap.java
+++ src/GUI/GameMap.java
@@ -11,23 +11,12 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.awt.Polygon;
-import java.awt.geom.Point2D;
 
 class GameMapScreen extends JFrame {
-    private static final int MAP_WIDTH = 25;
-    private static final int MAP_HEIGHT = 25;
+    private static final int MAP_WIDTH = 20;
+    private static final int MAP_HEIGHT = 20;
+    private static final int CELL_SIZE = 40; // Alapértelmezett cellaméret pixelben
     private static final double ZOOM_SPEED = 0.02;
-    private static final int CELL_SIZE = 40; // cell size in pixels
-
-    //hex
-    private static final double HEX_RADIUS = CELL_SIZE / 2.0;
-    private static final double HEX_SIDE = HEX_RADIUS * Math.sqrt(3) / 2.0;
-    private static final double HEX_HEIGHT = HEX_RADIUS;
-    private static final double HEX_WIDTH = 2 * HEX_SIDE;
-    private static final double HORIZONTAL_SPACING = HEX_WIDTH * 1.7;
-    private static final double VERTICAL_SPACING = HEX_HEIGHT * 0.9;
-
 
     private MapPanel mapPanel;
 
@@ -105,38 +94,49 @@ class GameMapScreen extends JFrame {
             if (backgroundImage != null) {
                 g2d.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
             } else {
+                // Ha nincs háttérkép, rajzolhatsz egy alapszínt
                 g2d.setColor(Color.gray);
                 g2d.fillRect(0, 0, getWidth(), getHeight());
             }
             g2d.transform(transform);
-        
-            for (int y = 0; y < MAP_HEIGHT; y++) {
-                for (int x = 0; x < MAP_WIDTH; x++) {
-                    Point2D.Double center = getHexagonCenter(x, y);
-                    Polygon hexagon = createHexagon(center.getX(), center.getY());
-        
-                    g2d.setColor(Color.WHITE);
-                    g2d.draw(hexagon);
-        
-                    g2d.setColor(Color.BLUE);
-                    for (Point tekton : tektonok) {
-                        if (tekton.x == x && tekton.y == y) {
-                            g2d.fill(hexagon);
-                            break;
-                        }
-                    }
-        
-                    g2d.setColor(Color.RED);
-                    int insectSize = (int) (HEX_RADIUS / 2);
-                    for (Point rovar : rovarok) {
-                        if (rovar.x == x && rovar.y == y) {
-                            g2d.fillOval((int) (center.getX() - insectSize / 2), (int) (center.getY() - insectSize / 2), insectSize, insectSize);
-                            break;
-                        }
-                    }
-                }
+
+            int panelWidth = getWidth();
+            int panelHeight = getHeight();
+            int mapPixelWidth = MAP_WIDTH * CELL_SIZE;
+            int mapPixelHeight = MAP_HEIGHT * CELL_SIZE;
+
+            // Kezdeti eltolás a térkép közepére igazításához
+            AffineTransform initialTransform = new AffineTransform();
+            initialTransform.translate(panelWidth / 2.0 - mapPixelWidth / 2.0,
+                                        panelHeight / 2.0 - mapPixelHeight / 2.0);
+
+            // Alkalmazzuk a zoom és mozgatás transzformációkat az eredeti után
+            AffineTransform combinedTransform = new AffineTransform(initialTransform);
+            combinedTransform.concatenate(transform);
+            g2d.transform(combinedTransform);
+
+            // Koordináta-rendszer rajzolása (opcionális)
+            g2d.setColor(Color.white);
+            for (int i = 0; i <= MAP_WIDTH; i++) {
+                g2d.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, MAP_HEIGHT * CELL_SIZE);
             }
-        
+            for (int j = 0; j <= MAP_HEIGHT; j++) {
+                g2d.drawLine(0, j * CELL_SIZE, MAP_WIDTH * CELL_SIZE, j * CELL_SIZE);
+            }
+
+            // Tektonok rajzolása
+            g2d.setColor(Color.BLUE);
+            for (Point tekton : tektonok) {
+                g2d.fillRect(tekton.x * CELL_SIZE, tekton.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+
+            // Rovarok rajzolása
+            g2d.setColor(Color.RED);
+            int insectSize = CELL_SIZE / 2;
+            for (Point rovar : rovarok) {
+                g2d.fillOval(rovar.x * CELL_SIZE + CELL_SIZE / 4, rovar.y * CELL_SIZE + CELL_SIZE / 4, insectSize, insectSize);
+            }
+
             g2d.dispose();
         }
 
@@ -166,12 +166,11 @@ class GameMapScreen extends JFrame {
         @Override
         public void mouseWheelMoved(MouseWheelEvent e) {
             double zoomFactor = 1.0 - e.getWheelRotation() * ZOOM_SPEED;
-            int centerX = getWidth() / 2;
-            int centerY = getHeight() / 2;
+            Point mousePoint = e.getPoint();
         
-            transform.translate(centerX, centerY);
+            transform.translate(mousePoint.x, mousePoint.y);
             transform.scale(zoomFactor, zoomFactor);
-            transform.translate(-centerX, -centerY);
+            transform.translate(-mousePoint.x, -mousePoint.y);
         
             repaint();
         }
@@ -186,26 +185,7 @@ class GameMapScreen extends JFrame {
         @Override
         public void mouseMoved(MouseEvent e) {}
     }
-    
-    private Point2D.Double getHexagonCenter(int x, int y) {
-        double centerX = x * HORIZONTAL_SPACING + HEX_SIDE;
-        double centerY = y * VERTICAL_SPACING + HEX_HEIGHT / 2;
-        if (y % 2 == 1) {
-            centerX += HORIZONTAL_SPACING / 2;
-        }
-        return new Point2D.Double(centerX, centerY);
-    }
-    
-    private Polygon createHexagon(double centerX, double centerY) {
-        Polygon hexagon = new Polygon();
-        for (int i = 0; i < 6; i++) {
-            double angle = 2 * Math.PI / 6 * i;
-            int px = (int) (centerX + HEX_RADIUS * Math.cos(angle));
-            int py = (int) (centerY + HEX_RADIUS * Math.sin(angle));
-            hexagon.addPoint(px, py);
-        }
-        return hexagon;
-    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new GameMapScreen());
     }
