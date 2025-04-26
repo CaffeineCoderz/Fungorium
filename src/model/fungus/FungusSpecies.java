2bd15689235f1a8abd3c64c4e60be009896c8e29
Diviki Levente <divikilevi13@gmail.com>
2025-04-26 18:12:00 +0200

    Utolsó spóraszórás bodyelhalás bug kijavítás

--- src/model/fungus/FungusSpecies.java
+++ src/model/fungus/FungusSpecies.java
@@ -338,7 +338,6 @@ public class FungusSpecies implements iControl {
     public void timeElapsed() {
         for (FungusBody body : bodies) {
             body.produceSpore();
-
             if (body.timeToDie()) {
                 destroyBody(body);
             }
@@ -349,17 +348,20 @@ public class FungusSpecies implements iControl {
     }
 
     public void timeElapsed(CommandProcessor cmdproc) {
+        List<FungusBody> removeBodies = new ArrayList();
         for (FungusBody body : bodies) {
-            body.produceSpore();
-
             if (body.timeToDie()) {
                 String objKey = cmdproc.findByObject(body);
                 if (objKey != null) {
                     cmdproc.getCreatedObjects().remove(objKey);
                 }
                 destroyBody(body);
+                removeBodies.add(body);
+            }else {
+                body.produceSpore();
             }
         }
+        bodies.removeAll(removeBodies);
         for (FungusThread thread : threads) {
             destroyThread(thread);
             String objKey = cmdproc.findByObject(thread);
