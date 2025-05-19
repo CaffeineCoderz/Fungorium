4aaa55df0ac2041160e419355da10b3c250c3314
Mador <dominik.andorka@gmail.com>
2025-05-19 21:17:36 +0200

    Kokemeny egy sor

--- src/GUI/FungoriumGUIBuilder.java
+++ src/GUI/FungoriumGUIBuilder.java
@@ -106,7 +106,8 @@ public class FungoriumGUIBuilder {
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(loadingPanel, BorderLayout.CENTER);
         frame.pack();
-        frame.setLocationRelativeTo(null);
+        // frame.setLocationRelativeTo(null);
+        frame.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - frame.getWidth()) / 2, 0); 
         frame.setResizable(false);
         frame.setVisible(true);
 
@@ -256,6 +257,7 @@ public class FungoriumGUIBuilder {
         JFrame frame = new JFrame("Fungorium Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ToolTipManager.sharedInstance().setInitialDelay(100);
+        frame.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - frame.getWidth()) / 2, 0);
         return frame;
     }
 
